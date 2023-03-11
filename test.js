533ab3aa90cdd1138b602b4b7f1ac0d0778d6549
sayedarmaans <124702127+sayedarmaans@users.noreply.github.com>
2023-03-11 09:00:23 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,73 @@
+// // Create an array with three elements and print out the second element.
+
+// let arr1 = [1, 2, 3];
+// console.log(arr1[1]);  
+
+
+// // Create an array with five elements and print out the length of the array.
+
+// const arr2 = [1, 2, 3, 4, 5];
+// console.log(arr2.length); 
+
+
+// // Create an array with four elements and print out each element using a for loop.
+
+// let arr3 = [1, 2, 3, 4];
+
+// for (let i = 0; i < arr3.length; i++) {
+//   console.log(arr3[i]);
+// }
+
+
+
+// // Create an array with four elements and remove the second element.
+
+// let array = [11, 22, 33, 44]; 
+// array.splice(1, 1);
+// console.log(array);
+ 
+
+// // Create an array with five elements and remove the last element.
+
+// let a = [18, 28, 98, 65, 77]; 
+// a.pop();
+// console.log(a);
+
+
+
+// // Create an array with four elements and sort the array in ascending order.
+
+// let b = [18, 28, 98, 65,];
+// let ans = b.sort(ascending);
+// function ascending(num1, num2) {
+// return num1 - num2;
+// }
+// console.log(ans);
+
+// // Create an array with five elements and sort the array in decending order.
+
+
+// let c = [18, 28, 98, 65,];
+// let ans1 = c.sort(decending);
+// function decending(num1, num2) {
+// return num2 - num1;
+// }
+// console.log(ans1);
+
+
+// //  Create an array with three elements and convert it to a string.
+
+// let i = [1, 2, 3,];
+// let str = i.toSting(); 
+// console.log(str);
+
+// Create an array with four elements and reverse the order of the elements.
+
+// let num = [1, 2, 3, 4,];
+// num.reverse();
+// console.log(num); 
+
+// Create an array with six elements and slice the array to create a new array with the first three elements
+// let num3 = [1, 2, 3, 4, 5, 6];
+// let ans = num3.slice(0,3);
+// console.log(ans);
