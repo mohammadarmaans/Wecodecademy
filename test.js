32843efee2c2e091e79f3e47c31fd67c74e6ad48
sayedarmaans <124702127+sayedarmaans@users.noreply.github.com>
2023-03-19 15:41:24 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,73 +1,70 @@
-// // Create an array with three elements and print out the second element.
 
-// let arr1 = [1, 2, 3];
-// console.log(arr1[1]);  
+// Write a JavaScript program to check whether a string "Code" presents at 5th (index 4) position in a given string, if "Code" presents in the string return the string without "Code" otherwise return the original one.
 
 
-// // Create an array with five elements and print out the length of the array.
-
-// const arr2 = [1, 2, 3, 4, 5];
-// console.log(arr2.length); 
-
-
-// // Create an array with four elements and print out each element using a for loop.
-
-// let arr3 = [1, 2, 3, 4];
-
-// for (let i = 0; i < arr3.length; i++) {
-//   console.log(arr3[i]);
-// }
+// Write a JavaScript program to capitalize the first letter of each word of a given string.
+// let str = "hello world!"; 
+// let ans = str.split(" ");
+// let ans1 = str.toUpperCase("0 ");
+// console.log(ans1); 
 
 
 
-// // Create an array with four elements and remove the second element.
+// Write a JavaScript function that reverse a number.
+//   let words = str.split(" ");
 
-// let array = [11, 22, 33, 44]; 
-// array.splice(1, 1);
-// console.log(array);
- 
+//   for (let i = 0; i < words.length; i++) {
+//     let firstLetter = words[i].charAt(0).toUpperCase();
 
-// // Create an array with five elements and remove the last element.
+//     let allLetter = words[i].slice(1);
 
-// let a = [18, 28, 98, 65, 77]; 
-// a.pop();
-// console.log(a);
+//     let capitalizedWord = firstLetter + allLetter;
 
+//     words[i] = capitalizedWord;
+//   }
 
+//   let capitalizedString = words.join(" ");
 
-// // Create an array with four elements and sort the array in ascending order.
+//   return capitalizedString;
 
-// let b = [18, 28, 98, 65,];
-// let ans = b.sort(ascending);
-// function ascending(num1, num2) {
-// return num1 - num2;
-// }
-// console.log(ans);
-
-// // Create an array with five elements and sort the array in decending order.
+// let inputString = "hello world";
+// let outputString = firstwordupper(inputString);
+// console.log(outputString);
 
 
-// let c = [18, 28, 98, 65,];
-// let ans1 = c.sort(decending);
-// function decending(num1, num2) {
-// return num2 - num1;
-// }
-// console.log(ans1);
+// Print below pattern
+// 1 2 3 4 5
+// 2 3 4 5
+//  3 4 5
+//   4 5
+//    5 
 
 
-// //  Create an array with three elements and convert it to a string.
+// for (let i = 1; i <= 5; i++) {
+//     let a = '';
+//     for (let j = i; j <= 5; j++) {
+//       a += j + ' ';
+//     }
+//     for (let k = 1; k < i; k++) {
+//       a = ' ' + a;
+//     }
+//     console.log(a);
+//   } 
 
-// let i = [1, 2, 3,];
-// let str = i.toSting(); 
-// console.log(str);
+// Write a JavaScript program to check whether all the digits in a given number are the same or not. 
 
-// Create an array with four elements and reverse the order of the elements.
 
-// let num = [1, 2, 3, 4,];
-// num.reverse();
-// console.log(num); 
+// function allDigitsSame(num) {
+//     let numString = num.toString();
+//     for (let i = 1; i < numString.length; i++) {
+//       if (numString[i] !== numString[0]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+//   let inputNum = 22222;
+//   let ans = allDigitsSame(inputNum);
+//   console.log(ans); 
 
-// Create an array with six elements and slice the array to create a new array with the first three elements
-// let num3 = [1, 2, 3, 4, 5, 6];
-// let ans = num3.slice(0,3);
-// console.log(ans);
+
